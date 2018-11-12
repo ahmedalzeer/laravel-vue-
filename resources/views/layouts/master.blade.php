230a2d29a5fcc7c21931c33c6a01370dbbc23bab
Your Name <az0192210255@gmail.com>
2018-11-11 21:24:10 -0800

    fix3 logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -42,10 +42,10 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="/" class="brand-link">
             <img src="/img/user.gif" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <span class="brand-text font-weight-light">Sales Managment</span>
         </a>
 
         <!-- Sidebar -->
